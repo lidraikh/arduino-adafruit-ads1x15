6296a29758e32d0c595b8a60d5371ab83c64598a
loiy <loiyedreekh@live.com>
2018-07-31 19:01:13 -0400

    rename ads_gain_t and rerrange ifndef

--- include/ads_gain_t.h
+++ include/ads_gain_t.h
@@ -1,25 +1,13 @@
 /**************************************************************************/
 /*!
-    @file     Adafruit_ADS1015.h
-    @author   K. Townsend (Adafruit Industries)
+    @file     adsGain_t.h
+    @author   
     @license  BSD (see license.txt)
 
-    This is a library for the Adafruit ADS1015 breakout board
-    ----> https://www.adafruit.com/products/???
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
-
-    @section  HISTORY
-
-    v1.0  - First release
-    v1.1  - Added ADS1115 support - W. Earl
 */
 /**************************************************************************/
-#ifndef _ADSGAIN_T_H_
-#define _ADSGAIN_T_H_
-
+#ifndef _ADS_GAIN_T_H_
+#define _ADS_GAIN_T_H_
 #define ADS1015_REG_CONFIG_PGA_MASK (0x0E00)
 #define ADS1015_REG_CONFIG_PGA_6_144V (0x0000) // +/-6.144V range = Gain 2/3
 #define ADS1015_REG_CONFIG_PGA_4_096V (0x0200) // +/-4.096V range = Gain 1
@@ -27,6 +15,7 @@
 #define ADS1015_REG_CONFIG_PGA_1_024V (0x0600) // +/-1.024V range = Gain 4
 #define ADS1015_REG_CONFIG_PGA_0_512V (0x0800) // +/-0.512V range = Gain 8
 #define ADS1015_REG_CONFIG_PGA_0_256V (0x0A00) // +/-0.256V range = Gain 16
+#endif 
 
 typedef enum
 {
